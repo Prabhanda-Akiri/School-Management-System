cc24aecfdfd0ec8d03f6ba54c3df5772c6843cfd
Sony <bachina.sony@gmail.com>
2019-03-27 21:50:41 +0530

    adding home data

---
+++ home/Aboutus.php
@@ -0,0 +1,131 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>home</title>
+<style>
+body {
+    background-color: #ffdead;
+    
+    border-color:red;
+}
+
+ul {	
+    position: -webkit-sticky; /* Safari */
+    position: sticky;
+    top: 0;
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+    background-color: #f3f3f3;
+}
+li {
+    float: left;
+    width:33%;
+    font-size: 18px;
+}
+li a {
+    display: block;
+    color: white;
+    text-align: center;
+   	padding: 14px 16px;
+ 	color: #666;
+    text-decoration: none;
+}
+
+li a:hover {
+    background-color:black ;
+    color:white;
+}
+.centered {
+	text-align:center;
+	font-family: Arial, Helvetica, sans-serif;
+    color:#ffdead;
+    position: absolute;
+    top: 80%;
+    left: 50%;
+    font-size: 31px;
+}
+
+.container {
+    position: -webkit-sticky; /* Safari */
+    position: sticky;
+    list-style-type: none;
+    overflow: hidden;
+    background-color: #f3f3f3;
+}
+.container2{
+	border-style: solid;
+    border-width: 2px;
+    border-color:#ffdead;
+    padding:50px;
+
+}
+.container4{
+    border-style: solid;
+    border-width: 2px;
+    border-color:#2e8b57;
+
+
+}
+.button1:hover {
+    background-color: white; /* Green */
+    color:#4CAF50;	
+}
+.button1 {
+	float:center;
+	width:32%;
+    background-color: #2e8b57; /* Green */
+    border: none;
+    color: white;
+    padding: 16px 32px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 20px;
+    margin: 4px 2px;
+    -webkit-transition-duration: 0.4s; /* Safari */
+    transition-duration: 0.4s;
+    cursor: pointer;
+}
+</style>
+</head>
+<body>
+
+
+
+<div class="container">
+<!-- <img src="image.png" alt="Norway" height="350" width="100%"> -->
+<div class="centered" ><b>STUDENT PROFILE MANAGEMENT SYSTEM</b></div>
+</div>
+<div class="container4">
+<ul>
+  <li style="float:left"><a class="active" href="home.php">Home</a></li>
+  <li><a href="Aboutus.php">About us</a></li>
+  <li><a href="contact.php">Contact us</a></li>
+</ul>
+</div>
+<p><h3>Our School aims to adopt the ever increasing competition and to frequently changing approach to learning. We proudly promise to make your dreams reality. We always update and upgrade our methodologies and our team from time to time to be on par with the reforms in the educational system.
+
+In concurrence with this we groom our students to challenge the CBSE, ICSE and IGCSE Boards with their immense knowledge bank gained through our "Olympiad Programme".
+</h3>
+</p>
+<p>
+    <h3>
+Our School was established in 2004 with a noble cause of grooming the students to see greater heights in their life and career. Our motto aims at holistic development of a student. We counsel every student to be self-responsible, well-disciplined, self-motivated & target-oriented so that they walk towards excellence in their academics and be a successful citizen of future India. The institution meticulously takes care of every aspect of lessons and trains each child to bring out innate talents.
+</h3>
+</p>
+<p>
+    <h3>
+A student who enters the portal of the school will find a challenging academic atmosphere and will have exposure to all kinds of intellectual, physical, co-curricular and extracurricular pursuits which makes learning whole some & rewarding. Students are given opportunities to develop their own talents as well as to be aware of national and global issues, while contributing to the healthy development of their own school campus and community.
+</h3>
+</p>
+
+<p>
+    <h3>
+The Olympiad programme is extensively useful to the students as it lays a strong foundation in the basic subjects like mathematics and science and nurtures them to achieve their goals. The school has developed some policies on making the students to be disciplined, responsible and meticulous in their regular study plan & continuous evaluation, through which they achieve excellent results in their academics and activities also. Continuous and comprehensive evaluation with regular CDF and IIT model tests in Mathematics, Physics, Chemistry is helping the students and the teachers to evaluate in a regular pace. Assessments are concept based and regular assignments and worksheets in other objects put the students into positive charging and energies.
+</h3>
+</p>
+</body>
+</html>
+
---
+++ home/home.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>home</title>
+<style>
+body {
+    background-color: #ffdead;
+    
+    border-color:red;
+}
+
+ul {	
+    position: -webkit-sticky; /* Safari */
+    position: sticky;
+    top: 0;
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+    background-color: #f3f3f3;
+}
+li {
+    float: left;
+    width:33%;
+    font-size: 18px;
+}
+li a {
+    display: block;
+    color: white;
+    text-align: center;
+   	padding: 14px 16px;
+ 	color: #666;
+    text-decoration: none;
+}
+
+li a:hover {
+    background-color:black ;
+    color:white;
+}
+.centered {
+	text-align:center;
+	font-family: Arial, Helvetica, sans-serif;
+    color:#ffdead;
+    position: absolute;
+    top: 80%;
+    left: 50%;
+    font-size: 31px;
+}
+
+.container {
+    position: -webkit-sticky; /* Safari */
+    position: sticky;
+    list-style-type: none;
+    overflow: hidden;
+    background-color: #f3f3f3;
+}
+.container2{
+	border-style: solid;
+    border-width: 2px;
+    border-color:#ffdead;
+    padding:50px;
+
+}
+.container4{
+    border-style: solid;
+    border-width: 2px;
+    border-color:#2e8b57;
+
+
+}
+.button1:hover {
+    background-color: white; /* Green */
+    color:#4CAF50;	
+}
+.button1 {
+	float:center;
+	width:32%;
+    background-color: #2e8b57; /* Green */
+    border: none;
+    color: white;
+    padding: 16px 32px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 20px;
+    margin: 4px 2px;
+    -webkit-transition-duration: 0.4s; /* Safari */
+    transition-duration: 0.4s;
+    cursor: pointer;
+}
+</style>
+</head>
+<body>
+
+
+
+<div class="container">
+<img src="image.png" alt="Norway" height="350" width="100%">
+<div class="centered" ><b>SCHOOL MANAGEMENT SYSTEM</b></div>
+</div>
+<div class="container4">
+<ul>
+  <li style="float:left"><a class="active" href="home.php">Home</a></li>
+  <li><a href="Aboutus.php">About us</a></li>
+  <li><a href="contact.php">Contact us</a></li>
+</ul>
+</div>
+<div class="container2">
+<a href="Adminlogin.php"><button class="button1">Admin</button></a>
+<a href="Parentlogin.php"><button class="button1">Parent</button></a>
+<a href="Teacherlogin.php"><button class="button1">Teacher</button></a>
+</div>
+
+</body>
+</html>
+
